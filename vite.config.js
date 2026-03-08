e2b155de71ba42a51ba18bf9d394bb4cffd31977
Steve Walsh <stevewalsh@smartretail.co>
2026-03-08 17:29:16 +0800

    count.sha version

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
 import { defineConfig } from "vite";
 import { execSync } from "child_process";
 
-function getGitCommitCount() {
+function getGitVersion() {
   try {
-    return execSync("git rev-list --count HEAD", { encoding: "utf-8" }).trim();
+    const count = execSync("git rev-list --count HEAD", {
+      encoding: "utf-8",
+    }).trim();
+    const sha = execSync("git rev-parse --short HEAD", {
+      encoding: "utf-8",
+    }).trim();
+    return `${count}.${sha}`;
   } catch {
     return null;
   }
 }
 
-const appVersion = getGitCommitCount() ?? "—";
+const appVersion = getGitVersion() ?? "—";
 
 export default defineConfig({
   base: "/flashback/",
